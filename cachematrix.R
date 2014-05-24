b0895e29f0d808c6ec227354788db1303c33755d
drcripptic <navin.sivanandam@gmail.com>
2014-05-24 11:51:24 +0100

    filled in cachematrix R script with required functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following pair of functions allow fast repeated computation of inverse 
+## matrices via caching. The first creates a new list object suitable for 
+## storing both matrix and cached inverse data if peviously calculated. The second
+## calculates inverse or returns cahce as necessary.
 
-## Write a short comment describing this function
+####
+
+## makeCacheMatrix reads in a matrix x and constructs a list of 4 functions: 
+## set (sets data to x), get (gets data x), setinverse (sets inverse of x), 
+## getinverse (gets inverse of x)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
+####
 
-## Write a short comment describing this function
+## cacheSolve inverts a matrix, first checking if the inverse has been previously 
+## calculated. it does this by taking the input matrix in the form of a makeCacheMatrix
+## list, which contains both the data and any previously calculated inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinverse(inv)
+  inv
 }
